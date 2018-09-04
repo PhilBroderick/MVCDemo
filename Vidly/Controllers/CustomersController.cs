0671e62a148d40e6575ad54c72a1770cdfcc335b
Phil Broderick <phil.broderick@forthcom.co.uk>
2018-09-04 16:55:40 +0100

    Udemy MVC Demo course

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.Models.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Phil Broderick" },
+                new Customer { Id = 2, Name = "Calvin Phillips" }
+            };
+
+            var viewModel = new CustomerListViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            return null;
+        }
+    }
+}
---
+++ Vidly/Models/ViewModels/CustomerListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models.ViewModels
+{
+    public class CustomerListViewModel
+    {
+        public List<Customer> Customers { get; set; }
+    }
+}
